2e1bcd381ba228670a1f8a056819873a90f81f1c
Dario Messina <nanodario@hotmail.co.uk>
2015-10-03 10:22:49 +0200

    Aggiornato nome programma in header GPLv2

--- src/VMTabSettings.h
+++ src/VMTabSettings.h
@@ -1,15 +1,15 @@
 /*
- * VBANT - VirtualBox Advanced Network Tool
+ * VB-ANT - VirtualBox - Advanced Network Tool
  * Copyright (C) 2015  Dario Messina
  *
- * This file is part of VBANT
+ * This file is part of VB-ANT
  *
- * VBANT is free software; you can redistribute it and/or
+ * VB-ANT is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * VBANT is distributed in the hope that it will be useful,
+ * VB-ANT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 /*
- * VBANT - VirtualBox Advanced Network Tool
+ * VB-ANT - VirtualBox - Advanced Network Tool
  * Copyright (C) 2015  Dario Messina
  *
- * This file is part of VBANT
+ * This file is part of VB-ANT
  *
- * VBANT is free software; you can redistribute it and/or
+ * VB-ANT is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * VBANT is distributed in the hope that it will be useful,
+ * VB-ANT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-// VBANT - VirtualBox Advanced Network Tool, Un tool per la didattica nel corso di reti di calcolatori
+// VB-ANT - VirtualBox - Advanced Network Tool, Un tool per la didattica nel corso di reti di calcolatori
 
 #include <QtGui/QApplication>
 #include "MainWindow.h"
